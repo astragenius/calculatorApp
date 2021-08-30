ccdd22ab6de241bf12749c0f5a18d9b8ad680b84
astragenius <darthmalak86@gmail.com>
2021-08-30 21:40:38 +0200

    optimized javascript toggle switch.

--- app/js/script.js
+++ app/js/script.js
@@ -10,32 +10,22 @@ toggleInput.forEach(input => {
 function switchTheme(e) {
 
     const html = document.querySelector('html');
-
+    html.classList.remove(html.classList[0]);
    
     if(e.target.value == "theme-1") {
 
-        html.classList.remove('theme-2');
-        html.classList.remove('theme-3')
         html.classList.add('theme-1');
         return
     }
     if(e.target.value == "theme-2") {
 
-        html.classList.remove('theme-1');
-        html.classList.remove('theme-3');
-
         html.classList.add('theme-2');
         return
     }
     if(e.target.value == "theme-3") {
-
-        html.classList.remove('theme-2');
-        html.classList.remove('theme-1');
         html.classList.add('theme-3');
+        return
     }
-
-    
-    
 
 }
 
